e086323c38a9c2868b9bf0282ce38708700a985b
Juan San Cristobal <130845154+Juan-MSCC@users.noreply.github.com>
2025-12-19 23:57:33 +0000

    Initial Commit. Finished fisrt part of the project

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronouns = ['the', 'our'];
+let adjs = ['great', 'big'];
+let nouns = ['jogger', 'racoon'];
+
+let domainNames = [];
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  pronouns.forEach((pronoun) => {
+    adjs.forEach((adj) => {
+      nouns.forEach((noun) => {
+        domainNames.push(pronoun + adj + noun + ".com")
+      })
+    })
+  })
+
+  console.log("------------------");
+  console.log("Possible domain names: ");
+
+  domainNames.forEach((name) => {console.log(name)})
+  
 };
